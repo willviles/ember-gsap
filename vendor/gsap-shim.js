ff0b1d1182c2ed7c5fea87c7ead77fc1033232cc
willviles <will@willviles.com>
2016-12-05 13:41:52 +0000

    Updates shim to be FastBoot compatible

--- vendor/gsap-shim.js
+++ vendor/gsap-shim.js
@@ -1,30 +1,64 @@
+/* global define, self */
+
 (function() {
 
+  function unavailableAttr(attr) {
+    var error = attr + " is not available. Either the bower package is missing or your app is in Fastboot Mode.";
+
+    if (attr === 'TweenLite' || attr === 'TweenMax' || attr === 'TimelineLite' || attr === 'TimelineMax') {
+      this.to = function() {
+        throw Error(error);
+      };
+    }
+
+    throw Error(error);
+  }
+
+  function checkAttrAvailable(attr) {
+    return self[attr] || unavailableAttr;
+  }
+
+  var availableAttrs = [
+    // Timelines
+    'TimelineLite', 'TimelineMax',
+    // Tweens
+    'TweenLite', 'TweenMax',
+    // Easing
+    'Power1', 'Power2', 'Power3', 'Power4', 'Back', 'Bounce', 'Circ', 'Cubic', 'Elastic', 'Expo', 'Linear', 'Sine', 'RoughEase', 'SlowMo', 'SteppedEase'
+  ];
+
+  var attrs = {};
+
+  for (var i = 0; i < availableAttrs.length; i++) {
+    var key = availableAttrs[i];
+    attrs[key] = checkAttrAvailable(key);
+  }
+
   var easing = {
-    'Power1': Power1,
-    'Power2': Power2,
-    'Power3': Power3,
-    'Power4': Power4,
-    'Back': Back,
-    'Bounce': Bounce,
-    'Circ': Circ,
-    'Cubic': Cubic,
-    'Elastic': Elastic,
-    'Expo': Expo,
-    'Linear': Linear,
-    'Sine': Sine,
-    'RoughEase': RoughEase,
-    'SlowMo': SlowMo,
-    'SteppedEase': SteppedEase
+    'Power1': attrs.Power1,
+    'Power2': attrs.Power2,
+    'Power3': attrs.Power3,
+    'Power4': attrs.Power4,
+    'Back': attrs.Back,
+    'Bounce': attrs.Bounce,
+    'Circ': attrs.Circ,
+    'Cubic': attrs.Cubic,
+    'Elastic': attrs.Elastic,
+    'Expo': attrs.Expo,
+    'Linear': attrs.Linear,
+    'Sine': attrs.Sine,
+    'RoughEase': attrs.RoughEase,
+    'SlowMo': attrs.SlowMo,
+    'SteppedEase': attrs.SteppedEase
   };
 
   define('gsap', [], function() {
     'use strict';
     return {
-      'TweenLite': TweenLite,
-      'TweenMax': TweenMax,
-      'TimelineLite': TimelineLite,
-      'TimelineMax': TimelineMax,
+      'TweenLite': attrs.TweenLite,
+      'TweenMax': attrs.TweenMax,
+      'TimelineLite': attrs.TimelineLite,
+      'TimelineMax': attrs.TimelineMax,
       'easing': easing
     };
   });
@@ -32,22 +66,22 @@
   /* globals define, GSAP */
   define('TweenLite', [], function() {
     'use strict';
-    return { 'default': TweenLite };
+    return { 'default': attrs.TweenLite };
   });
 
   define('TweenMax', [], function() {
     'use strict';
-    return { 'default': TweenMax };
+    return { 'default': attrs.TweenMax };
   });
 
   define('TimelineLite', [], function() {
     'use strict';
-    return { 'default': TimelineLite };
+    return { 'default': attrs.TimelineLite };
   });
 
   define('TimelineMax', [], function() {
     'use strict';
-    return { 'default': TimelineMax } ;
+    return { 'default': attrs.TimelineMax } ;
   });
 
   define('gsap/easing', [], function() {
